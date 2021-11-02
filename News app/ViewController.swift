35c118903d784145b195eb50f1b17bdc6ff3a4b3
Amrit Tiwari <tiwariammit@gmail.com>
2021-11-03 10:07:00 +1100

    Initial Commit

---
+++ News app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News app
+//
+//  Created by Amrit Tiwari on 03/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
